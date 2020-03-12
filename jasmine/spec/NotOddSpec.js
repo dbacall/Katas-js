31fda198ba4e4792517ae025fa57b67fce9ec47c
dbacall <dbacall@hotmail.co.uk>
2020-03-12 14:25:11 +0000

    test: add test for 0 edge case

--- jasmine/spec/NotOddSpec.js
+++ jasmine/spec/NotOddSpec.js
@@ -5,3 +5,7 @@ it('returns an empty array if given [1]', () => {
 it('returns [2] when given [1, 2]', () => {
   expect(notOdd([1,2])).toEqual([2])
 })
+
+it('returns [0, 4] when given [0, 3, 4]', () => {
+  expect(notOdd([0, 3, 4])).toEqual([0, 4])
+})

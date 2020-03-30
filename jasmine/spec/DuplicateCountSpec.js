ae8c8ca4ab0f77f899fcbe0bb6ff1f0e7d4ea905
dbacall <dbacall@hotmail.co.uk>
2020-03-30 15:18:02 +0100

    feat: duplicateCount can now recognize duplicates

--- jasmine/spec/DuplicateCountSpec.js
+++ jasmine/spec/DuplicateCountSpec.js
@@ -1,3 +1,7 @@
 it("returns 0 if there are no duplicates", () => {
   expect(duplicateCount("word")).toEqual(0);
 });
+
+it("returns 1 if there is 1 duplicate", () => {
+  expect(duplicateCount("speed")).toEqual(1);
+});
--- jasmine/src/DuplicateCount.js
+++ jasmine/src/DuplicateCount.js
@@ -1,3 +1,12 @@
 duplicateCount = word => {
-  return 0;
+  count = 0;
+  let letterArray = [];
+  word.split("").forEach(letter => {
+    if (letterArray.includes(letter)) {
+      count++;
+    } else {
+      letterArray.push(letter);
+    }
+  });
+  return count;
 };

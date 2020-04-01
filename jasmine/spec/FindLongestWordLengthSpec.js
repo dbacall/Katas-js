398517eb38c4c642e190882a7f1a945fbbe0e45a
dbacall <dbacall@hotmail.co.uk>
2020-04-01 16:36:08 +0100

    test: add test for longer strings for more rigorous testing

--- jasmine/spec/FindLongestWordLengthSpec.js
+++ jasmine/spec/FindLongestWordLengthSpec.js
@@ -2,6 +2,14 @@ it("returns the longest word length in a one word string", () => {
   expect(findLongestWordLength("hello")).toEqual(5);
 });
 
-it("returns the longest word length in a one word string", () => {
+it("returns the longest word length in a two word string", () => {
   expect(findLongestWordLength("hello worldie")).toEqual(7);
+});
+
+it("returns the longest word length in a long string", () => {
+  expect(
+    findLongestWordLength(
+      "What if we try a super-long word such as otorhinolaryngology"
+    )
+  ).toEqual(19);
 });
